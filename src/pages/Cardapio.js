830523da9c8a747f0a23c6d8be7132623bd78320
Gabriel <hsleirbag@hotmail.com>
2023-12-11 15:47:14 -0300

    Cardapio pronto

--- src/pages/Cardapio.js
+++ src/pages/Cardapio.js
@@ -5,7 +5,14 @@ import './main-cardapio.css';
 import TiposCard from "../components/TiposCard";
 import img from '../assets/caseiro.svg';
 import CardapioCard from "../components/CardapioCard";
-import carne from '../assets/image 11.png';
+import ParmegianaCarne from "../assets/img3-especialidade.png";
+import ParmegianaFrango from "../assets/parmegiana de frango.png";
+import YakisobaQueijo from "../assets/img2-especialidade.png";
+import YakisobaCarne from "../assets/yakisoba de carne.png";
+import YakisobaFrango from "../assets/yakisoba de frango.png";
+import StrogonoffCarne from "../assets/strogonoff de carne.png";
+import StrogonoffFrango from "../assets/strogonoff de frango.png";
+
 
 function Cardapio() {
     return (
@@ -26,24 +33,46 @@ function Cardapio() {
                         <img src={linhaTituloDireita} alt="" />
                     </div>
                 </section>
-                <section className="categoria">
-                    <div className="titulo-categoria">
+                <div className="titulo-categoria">
                         <h2><u>Yakisoba</u></h2>
                     </div>
+                <section className="categoria">
+    
                     <div classNames="itens">
-                        <CardapioCard img={carne} titulo="Yakisoba de Carne" descricao="Espaguete, cebola roxa, 
-cenoura, couve-flor, brócolis, 
-alcatra, repolho, champignon, " preco="15,90" />
+                        <CardapioCard img={YakisobaQueijo} titulo="Yakisoba de Queijo" descricao="Espaguete, cebola roxa, 
+cenoura, couve-flor, brócolis, ricota, 
+ repolho, champignon. " preco="15,90" />
+                    </div>
+                    <div classNames="itens">
+                        <CardapioCard img={YakisobaCarne} titulo="Yakisoba de Carne" descricao="Espaguete, cebola, 
+cenoura, pimentão, beterraba, carne, champignon. " preco="20,90" />
+                    </div>
+                    <div classNames="itens">
+                        <CardapioCard img={YakisobaFrango} titulo="Yakisoba de Frango" descricao="Espaguete, cebola roxa, 
+cenoura, pimentão, brócolis, frango, champignon. " preco="17,90" />
                     </div>
                 </section>
-                <section className="categoria">
-                    <div className="titulo-categoria">
+                <div className="titulo-categoria">
                         <h2><u>Parmegiana e Strogonoff</u></h2>
                     </div>
+                <section className="categoria">                  
                     <div className="itens">
-                        <CardapioCard img={carne} titulo="Parmegiana" descricao="Espaguete, cebola roxa, 
-cenoura, couve-flor, brócolis, 
-alcatra, repolho, champignon, " preco="24,90" />
+                        <CardapioCard img={ParmegianaCarne} titulo="Parmegiana de Carne" descricao="Filé mignon, cebola, 
+alho, manjericão, tomate, 
+queijo. " preco="24,90" />
+                    </div>
+                    <div className="itens">
+                        <CardapioCard img={ParmegianaFrango} titulo="Parmegiana de Frango" descricao="Filé de Frango, cebola, 
+alho, manjericão, tomate, 
+queijo. " preco="22,90" />
+                    </div>
+                    <div className="itens">
+                        <CardapioCard img={StrogonoffCarne} titulo="Strogonoff de Carne" descricao="Filé mignon, cebola, 
+limão, creme de leite, champignon, batata palha e arroz. " preco="21,90" />
+                    </div>
+                    <div className="itens">
+                        <CardapioCard img={StrogonoffFrango} titulo="Strogonoff de Frango" descricao="Filé de frango, cebola, 
+limão, creme de leite, champignon, batata palha e arroz. " preco="19,90" />
                     </div>
                 </section>
             </main>
